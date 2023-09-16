adbadd48e528d3db6611dec01bd7367b7ef5ad16
simeis147 <cpdong7@gmail.com>
2023-09-16 12:58:45 +0800

    DAY02

---
+++ health_parent/health_backend/src/main/java/com/example/controller/CheckItemController.java
@@ -0,0 +1,39 @@
+package com.example.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.example.constant.MessageConstant;
+import com.example.entity.Result;
+import com.example.pojo.CheckItem;
+import com.example.service.CheckItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 检查项管理
+ */
+@RestController
+@RequestMapping("/checkitem")
+public class CheckItemController {
+
+    @Reference
+    // 这个注解若是显示过期，使用@DubboReference，一样的效果
+    private CheckItemService checkItemService;
+
+    /**
+     * 新增检查项
+     * @param checkItem
+     * @return
+     */
+    @RequestMapping("/add")
+    public Result add(@RequestBody CheckItem checkItem){
+        try {
+            checkItemService.add(checkItem);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, MessageConstant.ADD_CHECKITEM_FAIL);
+        }
+        return new Result(true, MessageConstant.ADD_CHECKITEM_SUCCESS);
+    }
+}
---
+++ health_parent/health_interface/src/main/java/com/example/service/CheckItemService.java
@@ -0,0 +1,11 @@
+package com.example.service;
+
+import com.example.pojo.CheckItem;
+
+/**
+ * 服务接口
+ */
+public interface CheckItemService {
+
+    public void add(CheckItem checkItem);
+}
